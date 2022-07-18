f6775899f0b2aef556040a31190805512a6fa9e0
Carlos Zabala <czabala847@gmail.com>
2022-07-18 15:04:38 -0500

    Terminando pipes

--- src/app/sales/pages/no-commons/no-commons.component.ts
+++ src/app/sales/pages/no-commons/no-commons.component.ts
@@ -5,15 +5,47 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './no-commons.component.html',
 })
 export class NoCommonsComponent implements OnInit {
-  name: string = 'Ana';
-  gender: string = 'femenino';
+  //i18nSelectPipe
+  name: string = 'Carlos';
+  gender: string = 'masculino';
 
   invitationMap = {
     femenino: 'invitarla',
     masculino: 'invitarlo',
   };
 
+  client = {
+    name: 'Carlos',
+    age: 25,
+    address: 'Barranquilla, Colombia',
+  };
+
+  //i18nPluralPipe
+
+  clients: string[] = ['Carlos', 'Elena', 'Ana'];
+  clientsMap = {
+    '=0': 'cero clientes esperando.',
+    '=1': 'un cliente esperando.',
+    other: '# clientes esperando.',
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  changeClient() {
+    if (this.gender === 'masculino') {
+      (this.name = 'Ana'), (this.gender = 'femenino');
+    } else {
+      (this.name = 'Carlos'), (this.gender = 'masculino');
+    }
+  }
+
+  removeClient() {
+    // if (this.clients.length > 0) {
+    //   this.clients = this.clients.splice(0, this.clients.length - 1);
+    // }
+
+    this.clients.pop();
+  }
 }
