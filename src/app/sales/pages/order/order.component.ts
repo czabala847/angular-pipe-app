a051459be468c1173f5403f4cb70f95db69e4dd2
Carlos Zabala <czabala847@gmail.com>
2022-07-19 09:01:26 -0500

    Llenando tabla de heroes

--- src/app/sales/pages/order/order.component.ts
+++ src/app/sales/pages/order/order.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Hero, Color } from '../../interfaces/sales.interface';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent {
   text: string = 'Nosotros';
   mayus: boolean = true;
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  heroes: Hero[] = [
+    {
+      name: 'Superman',
+      fly: true,
+      color: Color.blue,
+    },
+    {
+      name: 'Batman',
+      fly: false,
+      color: Color.black,
+    },
+    {
+      name: 'Robin',
+      fly: false,
+      color: Color.green,
+    },
+    {
+      name: 'Daredevil',
+      fly: false,
+      color: Color.red,
+    },
+    {
+      name: 'Linterna Verde',
+      fly: true,
+      color: Color.green,
+    },
+  ];
 
   toggleMayus() {
     this.mayus = !this.mayus;
